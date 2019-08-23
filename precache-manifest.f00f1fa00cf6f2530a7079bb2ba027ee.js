3d4ec07903ea016a0c5f3e13c48fe6343427846f
Marek <jonas.wu@waystorm.com>
2019-08-23 12:17:31 +0800

    Updates

---
+++ precache-manifest.f00f1fa00cf6f2530a7079bb2ba027ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2aefa3593f14065ad525f386f9cf430a",
+    "url": "/My-First-Profolio/index.html"
+  },
+  {
+    "revision": "88ae0a292bcac09906c9",
+    "url": "/My-First-Profolio/static/css/2.60a6b8e0.chunk.css"
+  },
+  {
+    "revision": "965c56e9983c61cc3751",
+    "url": "/My-First-Profolio/static/css/main.7a6c7b56.chunk.css"
+  },
+  {
+    "revision": "88ae0a292bcac09906c9",
+    "url": "/My-First-Profolio/static/js/2.9d8ef437.chunk.js"
+  },
+  {
+    "revision": "965c56e9983c61cc3751",
+    "url": "/My-First-Profolio/static/js/main.61fd044b.chunk.js"
+  },
+  {
+    "revision": "09e4e9cf5b8d8b296208",
+    "url": "/My-First-Profolio/static/js/runtime~main.5fcf5ee9.js"
+  }
+]);
